8f1b3246dea9fbae5aa9f9c36908678096494934
deektionary <deekshagoyal19@gmail.com>
2017-11-12 22:10:00 -0800

    hashtag

--- navigation/MainTabNavigator.js
+++ navigation/MainTabNavigator.js
@@ -8,6 +8,7 @@ import Colors from '../constants/Colors';
 import HomeScreen from '../screens/HomeScreen';
 import LinksScreen from '../screens/LinksScreen';
 import SettingsScreen from '../screens/SettingsScreen';
+import NotableScreen from '../screens/NotableScreen';
 
 export default TabNavigator(
   {
@@ -19,6 +20,9 @@ export default TabNavigator(
     },
     Settings: {
       screen: SettingsScreen,
+    },
+    Notable: {
+      screen: NotableScreen,
     },
   },
   {
